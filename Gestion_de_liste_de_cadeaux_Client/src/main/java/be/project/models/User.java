eb01de94c39e1f0c4586f608ceb30819ed239732
Emmanuel <dimilime.soukouya@condorcet.be>
2022-12-13 15:50:29 +0100

    project skeleton first part

---
+++ Gestion_de_liste_de_cadeaux_Client/src/main/java/be/project/models/User.java
@@ -0,0 +1,22 @@
+package be.project.models;
+
+import java.io.Serializable;
+
+public class User implements Serializable{
+
+	private static final long serialVersionUID = -5899409470895546883L;
+	
+	private int userId;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+	
+	public User() {
+	}
+	
+	
+	
+	
+
+}
